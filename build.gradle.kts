05a4e9211fc196e1e9904c2d7e5cffb0afd1e4aa
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2019-12-08 01:01:33 +0100

    adding gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.springframework.boot") version "2.2.2.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	id("com.adarshr.test-logger") version "2.0.0"
+	id("com.avast.gradle.docker-compose") version "0.10.4"
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
 }
@@ -41,4 +44,19 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Wrapper> {
 	gradleVersion = "6.0.1"
+}
+
+testlogger {
+	theme = MOCHA
+	showFullStackTraces = true
+	showStandardStreams = true
+}
+
+dockerCompose.isRequiredBy(tasks.test.get())
+dockerCompose {
+	useComposeFiles = listOf("docker-compose.yml")
+	captureContainersOutput = false
+	stopContainers = true
+	removeContainers = true
+	removeOrphans = true
 }
